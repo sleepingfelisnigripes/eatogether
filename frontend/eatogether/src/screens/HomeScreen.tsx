cc2b16c2a5e6c855135f149b3bc36345244c8749
Jeffrey Lau <jeffrey@bishouse.com>
2022-09-24 01:51:49 +1000

    Updated map logic

--- frontend/eatogether/src/screens/HomeScreen.tsx
+++ frontend/eatogether/src/screens/HomeScreen.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { LatLng, Region } from "react-native-maps";
 import * as Location from "expo-location";
 
 interface LocationObjectCoords {
@@ -29,6 +29,9 @@ export default function HomeScreen() {
 
   let positionWatcher: Location.LocationSubscription;
   const mapViewRef = useRef<MapView>(null);
+  const [region, setRegion] = useState<Region | null>(null);
+  const [followMe, setFollowMe] = useState<boolean>(false);
+  const trackingTimer = useRef<NodeJS.Timer>();
 
   // Get user's permission for location service once the screen is launched
   useEffect(() => {
@@ -37,51 +40,82 @@ export default function HomeScreen() {
       if (status !== "granted") {
         return;
       }
-      // If location service permission is granted
-      //   await updateUserPosition();
-      positionWatcher = await Location.watchPositionAsync(
-        {
-          accuracy: Location.Accuracy.Low,
-          timeInterval: 10000,
-          distanceInterval: 0,
-        },
-        (data) => {
-          //   console.log(data);
-          const newRegion = {
-            latitudeDelta: mapZoom,
-            longitudeDelta: mapZoom,
-            latitude: data.coords.latitude,
-            longitude: data.coords.longitude,
-          };
-          mapViewRef.current?.animateToRegion(newRegion, 300);
-        }
-      );
+      updateUserPosition();
     })();
   }, []);
 
+  useEffect(() => {
+    if (followMe) {
+      updateUserPosition();
+      trackingTimer.current = setInterval(async () => {
+        await updateUserPosition();
+      }, 3000);
+    } else {
+      if (trackingTimer.current) {
+        clearInterval(trackingTimer.current);
+      }
+    }
+
+    return () => {
+      clearInterval(trackingTimer.current);
+    };
+  }, [followMe]);
+
   async function updateUserPosition() {
-    const latestPosition = await Location.getCurrentPositionAsync({});
-    const newRegion = {
+    const latestPosition: Location.LocationObject =
+      await Location.getCurrentPositionAsync({
+        accuracy: Location.Accuracy.Low,
+      });
+    const newRegion: Region = {
       latitudeDelta: mapZoom,
       longitudeDelta: mapZoom,
       latitude: latestPosition.coords.latitude,
       longitude: latestPosition.coords.longitude,
     };
-    mapViewRef.current?.animateToRegion(newRegion, 300);
+    const newPosition: LatLng = {
+      latitude: latestPosition.coords.latitude,
+      longitude: latestPosition.coords.longitude,
+    };
+    // mapViewRef.current?.animateToRegion(newRegion, 300);
+    mapViewRef.current?.animateCamera(
+      { center: newPosition },
+      { duration: 300 }
+    );
+    // setRegion(newRegion);
   }
 
   return (
     <>
       <MapView
+        provider={"google"}
         ref={mapViewRef}
         style={styles.mapContainer}
         initialRegion={UNIMELB_REGION}
+        showsUserLocation={true}
+        // followsUserLocation={true}
+        showsMyLocationButton={false}
+        showsPointsOfInterest={false}
+        pitchEnabled={false}
+        region={region!}
       />
-      <Button title="Update location" onPress={updateUserPosition} />
+      <Button
+        title={`Follow me: ${followMe ? "ON" : "OFF"}`}
+        onPress={() => {
+          setFollowMe(followMe ? false : true);
+        }}
+      />
+      <Button
+        title="Show my current location"
+        onPress={() => {
+          (async () => {
+            await updateUserPosition();
+          })();
+        }}
+      />
     </>
   );
 }
-console.log("rendered");
+console.log("Rendered Home Screen");
 
 const styles = StyleSheet.create({
   mapContainer: {
