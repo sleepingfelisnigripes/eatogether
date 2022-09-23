bcd533f5d6ae6268ed041ef82f6fd183712bafb4
Jeffrey Lau <jeffrey@bishouse.com>
2022-09-23 18:18:13 +1000

    Add icons to tabs

--- frontend/eatogether/App.tsx
+++ frontend/eatogether/App.tsx
@@ -6,12 +6,37 @@ import ProfileScreen from "./src/screens/ProfileScreen";
 import HomeScreen from "./src/screens/HomeScreen";
 import RestaurantScreen from "./src/screens/RestaurantScreen";
 import MyGroupsScreen from "./src/screens/MyGroupsScreen";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
+type IconName = React.ComponentProps<typeof Ionicons>["name"];
 
 function MyTabs() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName: IconName = "home";
+
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Restaurant") {
+            iconName = focused ? "restaurant" : "restaurant-outline";
+          } else if (route.name === "My Groups") {
+            iconName = focused
+              ? "chatbubble-ellipses"
+              : "chatbubble-ellipses-outline";
+          } else if (route.name === "Profile") {
+            iconName = focused ? "person-circle" : "person-circle-outline";
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "blue",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Restaurant" component={RestaurantScreen} />
       <Tab.Screen name="My Groups" component={MyGroupsScreen} />
