c870f09f28a03e4a7edfd6be3d35af4085c8321b
Jeffrey Lau <jeffrey@bishouse.com>
2022-09-23 23:06:35 +1000

    Updated map to follow user's position

--- frontend/eatogether/src/screens/HomeScreen.tsx
+++ frontend/eatogether/src/screens/HomeScreen.tsx
@@ -1,9 +1,92 @@
-import { View, Text } from "react-native";
+import { useEffect, useState, useRef } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import MapView from "react-native-maps";
+import * as Location from "expo-location";
+
+interface LocationObjectCoords {
+  accuracy?: number | null;
+  altitude?: number | null;
+  altitudeAccuracy?: number | null;
+  heading?: number | null;
+  latitude: number;
+  longitude: number;
+  speed?: number | null;
+}
+interface LocationObject {
+  coords: LocationObjectCoords;
+  mocked?: boolean;
+  timestamp?: number;
+}
 
 export default function HomeScreen() {
+  const mapZoom: number = 0.004;
+  const UNIMELB_REGION = {
+    latitude: -37.796671094693004,
+    longitude: 144.96108337210148,
+    latitudeDelta: mapZoom,
+    longitudeDelta: mapZoom,
+  };
+
+  let positionWatcher: Location.LocationSubscription;
+  const mapViewRef = useRef<MapView>(null);
+
+  // Get user's permission for location service once the screen is launched
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        return;
+      }
+      // If location service permission is granted
+      //   await updateUserPosition();
+      positionWatcher = await Location.watchPositionAsync(
+        {
+          accuracy: Location.Accuracy.Low,
+          timeInterval: 10000,
+          distanceInterval: 0,
+        },
+        (data) => {
+          //   console.log(data);
+          const newRegion = {
+            latitudeDelta: mapZoom,
+            longitudeDelta: mapZoom,
+            latitude: data.coords.latitude,
+            longitude: data.coords.longitude,
+          };
+          mapViewRef.current?.animateToRegion(newRegion, 300);
+        }
+      );
+    })();
+  }, []);
+
+  async function updateUserPosition() {
+    const latestPosition = await Location.getCurrentPositionAsync({});
+    const newRegion = {
+      latitudeDelta: mapZoom,
+      longitudeDelta: mapZoom,
+      latitude: latestPosition.coords.latitude,
+      longitude: latestPosition.coords.longitude,
+    };
+    mapViewRef.current?.animateToRegion(newRegion, 300);
+  }
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home</Text>
-    </View>
+    <>
+      <MapView
+        ref={mapViewRef}
+        style={styles.mapContainer}
+        initialRegion={UNIMELB_REGION}
+      />
+      <Button title="Update location" onPress={updateUserPosition} />
+    </>
   );
 }
+console.log("rendered");
+
+const styles = StyleSheet.create({
+  mapContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
