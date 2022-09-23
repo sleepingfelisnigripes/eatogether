56ecbb9a63aae9036576d31aeec4f6a873f449d7
Jeffrey Lau <jeffrey@bishouse.com>
2022-09-23 18:21:56 +1000

    Remove header

--- frontend/eatogether/App.tsx
+++ frontend/eatogether/App.tsx
@@ -33,7 +33,8 @@ function MyTabs() {
           // You can return any component that you like here!
           return <Ionicons name={iconName} size={size} color={color} />;
         },
-        tabBarActiveTintColor: "blue",
+        headerShown: false,
+        tabBarActiveTintColor: "teal",
         tabBarInactiveTintColor: "gray",
       })}
     >
