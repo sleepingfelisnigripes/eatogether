454effdec685e4dd7dbfa7d36f2ced2230c8fbe2
Jeffrey Lau <jeffrey@bishouse.com>
2022-10-12 23:18:36 +1100

    Updated APIs

--- frontend/eatogether/api/Common.ts
+++ frontend/eatogether/api/Common.ts
@@ -1,1 +1,8 @@
 export const API_URL:string = 'https://api.eatogether.site';
+
+export type ServerResponse = {
+    data?: any;
+    timestamp?: string;
+    status?: string;
+    message?: string;
+}
