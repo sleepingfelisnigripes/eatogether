b062a685dba380937ee0572a2fec3a827d6c1766
Jeffrey Lau <jeffrey@bishouse.com>
2022-10-16 15:59:52 +1100

    Add stream-chat-expo

--- frontend/eatogether/App.tsx
+++ frontend/eatogether/App.tsx
@@ -7,8 +7,8 @@ import HomeScreen from "./src/screens/HomeScreen";
 import RestaurantsScreen from "./src/screens/RestaurantsScreen";
 import MyGroupsScreen from "./src/screens/MyGroupsScreen";
 import { Ionicons } from "@expo/vector-icons";
-import { Provider } from 'react-redux';
-import store from './src/redux/store';
+import { Provider } from "react-redux";
+import store from "./src/redux/store";
 
 type IconName = React.ComponentProps<typeof Ionicons>["name"];
 
--- frontend/eatogether/src/screens/MyGroupsScreen.tsx
+++ frontend/eatogether/src/screens/MyGroupsScreen.tsx
@@ -1,9 +1,95 @@
-import { View, Text } from "react-native";
+import { useEffect, useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { StreamChat, Channel as ChannelType } from "stream-chat";
+import {
+  Channel,
+  ChannelList,
+  Chat,
+  MessageInput,
+  MessageList,
+  MessageType,
+  OverlayProvider,
+  Thread,
+} from "stream-chat-expo";
+
+const client = StreamChat.getInstance("vsw2j53wvgv6");
 
 export default function MyGroupsScreen() {
+  const [channel, setChannel] = useState<ChannelType>();
+  const [clientReady, setClientReady] = useState(false);
+  const [thread, setThread] = useState<MessageType | null>();
+
+  useEffect(() => {
+    const setupClient = async () => {
+      try {
+        await client.connectUser(
+          {
+            id: "jlahey",
+            name: "Jim Lahey",
+            image: "https://i.imgur.com/fR9Jz14.png",
+          },
+          "user_token"
+        );
+        setClientReady(true);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    setupClient();
+  }, []);
+
+  const onBackPress = () => {
+    if (thread) {
+      setThread(undefined);
+    } else if (channel) {
+      setChannel(undefined);
+    }
+  };
+
+  if (!clientReady) return null;
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>My Groups</Text>
-    </View>
+    <OverlayProvider topInset={60}>
+      <TouchableOpacity onPress={onBackPress} disabled={!channel}>
+        <View style={{ height: 60, paddingLeft: 16, paddingTop: 40 }}>
+          {channel && <Text>Back</Text>}
+        </View>
+      </TouchableOpacity>
+      <View style={{ flex: 1 }}>
+        <Chat client={client}>
+          {channel ? (
+            <Channel
+              channel={channel}
+              keyboardVerticalOffset={60}
+              thread={thread}
+              threadList={!!thread}
+            >
+              {thread ? (
+                <Thread />
+              ) : (
+                <>
+                  <MessageList onThreadSelect={setThread} />
+                  <MessageInput />
+                </>
+              )}
+            </Channel>
+          ) : (
+            <ChannelList onSelect={setChannel} />
+          )}
+        </Chat>
+      </View>
+    </OverlayProvider>
   );
+
+  // return (
+  //   <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+  //     <OverlayProvider>
+  //       <Chat client={client}>
+  //         <ChannelList />
+  //       </Chat>
+  //       ;
+  //     </OverlayProvider>
+  //   </View>
+  // );
 }
