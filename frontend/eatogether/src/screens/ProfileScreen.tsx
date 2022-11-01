f05887422075d4c26e22973f2b1aaca525b1829f
sleepingfelisnigripes <sophielinfeicao@gmail.com>
2022-11-01 15:54:46 +1100

    profile screen font update

--- frontend/eatogether/src/screens/ProfileScreen.tsx
+++ frontend/eatogether/src/screens/ProfileScreen.tsx
@@ -203,7 +203,7 @@ const styles = StyleSheet.create({
     color: "#370617",
     textAlign: "center",
     textAlignVertical: "center",
-    fontFamily: "Futura",
+    //fontFamily: "Futura",
     //backgroundColor: '#FFFFFF',
   },
 
@@ -225,7 +225,7 @@ const styles = StyleSheet.create({
     fontSize: 22,
     color: "#FFFFFF",
     fontWeight: "700",
-    fontFamily: "Futura",
+    //fontFamily: "Futura",
   },
 
   // Set ID format
@@ -236,7 +236,7 @@ const styles = StyleSheet.create({
     /* position: 'absolute',
     left: 145,
     top: 110, */
-    fontFamily: "Futura",
+    //fontFamily: "Futura",
     //backgroundColor: 'transparent',
   },
 
@@ -307,7 +307,7 @@ const styles = StyleSheet.create({
     fontSize: 17,
     fontWeight: "500",
     color: "#370617",
-    fontFamily: "Futura",
+    //fontFamily: "Futura",
     //paddingLeft: 15,
     marginLeft: 15,
     marginTop: 2,
